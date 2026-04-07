8cbebd179d8f40c2656abbb7331c86c858eb7d9d
welt <welt@mac.com>
2026-04-07 20:56:20 +0100

    adds test for hold

--- order.test.js
+++ order.test.js
@@ -4,17 +4,24 @@ import Order from "./order.js";
 import Customer from "./customer.js";
 import Product from "./product.js";
 
-test("Sufficient product stock", () => {
-  const order = new Order();
+// test("Sufficient product stock", () => {
+//   const order = new Order();
 
-  assert.strictEqual(order.items.length === 0, true);
+//   assert.strictEqual(order.items.length === 0, true);
 
-  const customer = new Customer()
+//   const customer = new Customer();
 
-  assert.ok(customer)
+//   assert.ok(customer);
 
+//   // a temporary hold for the sale quantity is placed on that product’s stock,
+//   const product = new Product();
 
-  // a temporary hold for the sale quantity is placed on that product’s stock,
-  const product = new Product()
-  assert.ok(product)
+//   product.hold = true;
+//   assert.strictEqual(product.hold, true);
+// });
+
+test("temporary hold on product stock", () => {
+  const product = new Product();
+  product.hold = 1;
+  assert.strictEqual(product.hold, 1);
 });
