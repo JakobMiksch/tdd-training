f53c0b5b24dda905ba69b63c5a63848e4e9eea98
welt <welt@mac.com>
2026-04-07 20:10:58 +0100

    moves order class to own file

--- order.test.js
+++ order.test.js
@@ -1,12 +1,6 @@
 import test from "node:test";
-
 import assert from "node:assert/strict";
-
-class Order {
-  constructor() {
-    this.items = [];
-  }
-}
+import Order from "./order.js";
 
 test("order is empty", () => {
   const order = new Order();
