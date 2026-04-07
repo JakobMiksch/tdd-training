f7c622d4a960233fb9fbffbd37d9bc929b3a0b3a
Jakob Miksch <info@jakobmiksch.eu>
2026-04-07 21:22:19 +0200

    Add customer exist condition

--- order.test.js
+++ order.test.js
@@ -1,9 +1,14 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import Order from "./order.js";
+import Customer from "./customer.js";
 
 test("order is empty", () => {
   const order = new Order();
 
   assert.strictEqual(order.items.length === 0, true);
+
+  const customer = new Customer()
+
+  assert.ok(customer, true)
 });
